78eb4b3a002f5ed87c4d905098ffad41c6995e6f
ndigitals04 <ndigitals001@gmail.com>
2025-09-03 21:25:10 +0100

    Updated insert function
    Insert function now creates a new array and copies old data from old array into it anytime the length of the array has been maxed.

--- arrays.py
+++ arrays.py
@@ -5,17 +5,28 @@ class Array:
         self.length = length
         self.items = [None] * length
         self.count = 0
-        for item in self.items:
-            if item != None:
-                self.count += 1
+        # for item in self.items:
+        #     if item != None:
+        #         self.count += 1
     
     def print(self):
         for i in range(self.count):
             print(self.items[i])
     
     def insert(self, value):
-        self.items[self.count] = value
-        self.count += 1
+        if self.count == self.length:
+            self.length = (self.length + round(self.length * .5))
+            newArray = [None] * self.length
+            for i in range(self.count):
+                newArray[i] = self.items[i]
+            self.items = newArray
+            print("new array initialised")
+            self.items[self.count] = value
+            self.count += 1
+            
+        else:
+            self.items[self.count] = value
+            self.count += 1
     
     def removeAt(self, index):
         if index < 0 or index >= self.count:
@@ -24,12 +35,16 @@ class Array:
             self.items[i] = self.items[i + 1]
         self.items[self.count - 1] = None
         self.count -= 1
-
+        
 array = Array(5) #Creates an array of length 5
 array.insert(1) #Inserts 1 into the array
 array.insert(2) #Inserts 2 into the array
 array.insert(3) #Inserts 3 into the array
 array.insert(4) #Inserts 4 into the array
+array.insert(5) #Inserts 5 into the array
+array.insert(6) #Inserts 6 into the array, resizes the array
+array.insert(7) #Inserts 7 into the array
+# array.insert(8)
 array.removeAt(3) #Removes the element at index 3
 
 array.print() #Prints the array
